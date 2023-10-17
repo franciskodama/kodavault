3c416fcafd7e2e79bc1c9685bc5ddd237915549a
Francis Kodama <fk@fkodama.com>
2023-10-17 19:24:16 -0400

    [WIP] Home

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,12 @@ export default async function Home() {
   return (
     <main className='flex h-screen flex-col items-center justify-between p-14'>
       <div className='z-10 max-w-5xl w-full h-full items-center justify-between font-mono text-sm border-2'>
-        SERVER:
+        <div className='mb-32'>{`layout --> page.tsx`}</div>
         {session?.user?.name ? (
           <div>{session?.user?.name}</div>
         ) : (
           <div>Not logged in</div>
         )}
-        Hello World!
       </div>
     </main>
   );
