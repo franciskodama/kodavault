9b4d20fa46ec67c0b5d2165116c776a185e81a0d
Francis Kodama <fk@fkodama.com>
2023-10-19 16:20:03 -0400

    Stock unique file

--- app/lib/stock.server.ts
+++ app/lib/stock.server.ts
@@ -10,3 +10,14 @@ export const getStockBr = async (symbol: string) => {
     return { error };
   }
 };
+
+export const getStock = async (symbol: string) => {
+  try {
+    const response = await fetch(
+      `https://serpapi.com/search.json?engine=google_finance&q=${symbol}&api_key=${process.env.NEXT_PUBLIC_SERPAPI}`
+    ).then((res) => res.json());
+    return response;
+  } catch (error) {
+    return { error };
+  }
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import { getServerSession } from 'next-auth';
-import { getStockBr } from './lib/stock-br.server';
+import { getStockBr } from './lib/stock.server';
 
 export default async function Home() {
   const session = await getServerSession();
