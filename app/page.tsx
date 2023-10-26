30ca990a6db477bff92f822469d1945b6d672310
Francis Kodama <fk@fkodama.com>
2023-10-25 20:42:53 -0400

    Delete tests

--- app/page.tsx
+++ app/page.tsx
@@ -16,21 +16,6 @@ export default async function Home() {
       <div className='z-10 max-w-5xl w-full h-full items-center justify-between font-mono text-sm border-2'>
         <div className='mb-32'>{`layout --> page.tsx`}</div>
         <div>IVVB11:{stockBr && stockBr.futures_chain[0].price}</div>
-
-        {/* 
-        {session?.user?.name ? (
-          <div
-            className='text-2xl'
-            style={{
-              fontFamily: 'gabarito',
-            }}
-          >
-            {session?.user?.name}
-          </div>
-        ) : (
-          <div>Not logged in</div>
-        )} */}
-
         {session?.user?.name ? (
           <div
             className='text-2xl'
