61abdb7dc1619a36238da89559e1290d17763300
Francis Kodama <fk@fkodama.com>
2023-10-28 19:14:08 -0400

    Get prices

--- app/lib/crypto.server.ts
+++ app/lib/crypto.server.ts
@@ -1,7 +1,8 @@
-export const getCryptos = async () => {
+export const getCryptos = async (symbol: string | null) => {
   try {
     const response = await fetch(
-      'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
+      // 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
+      `https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=${symbol}`,
       {
         headers: {
           'X-CMC_PRO_API_KEY': process.env.NEXT_PUBLIC_COINMARKETCAP_KEY || '',
