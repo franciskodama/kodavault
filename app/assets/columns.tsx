e4a25cb58c715ea43a40a8bc6667390b082bcd3b
Francis Kodama <fk@fkodama.com>
2023-10-29 13:31:57 -0400

    New db schema

--- app/assets/columns.tsx
+++ app/assets/columns.tsx
@@ -11,6 +11,10 @@ export const columns: ColumnDef<Asset>[] = [
     header: () => <div className={headerClass}>Wallet</div>,
   },
   {
+    accessorKey: 'account',
+    header: () => <div className={headerClass}>Account</div>,
+  },
+  {
     accessorKey: 'asset',
     header: () => <div className={headerClass}>Asset</div>,
   },
@@ -43,5 +47,13 @@ export const columns: ColumnDef<Asset>[] = [
   {
     accessorKey: 'type',
     header: () => <div className={headerClass}>Type</div>,
+  },
+  {
+    accessorKey: 'subtype',
+    header: () => <div className={headerClass}>Subtype</div>,
+  },
+  {
+    accessorKey: 'currency',
+    header: () => <div className={headerClass}>Currency</div>,
   },
 ];
