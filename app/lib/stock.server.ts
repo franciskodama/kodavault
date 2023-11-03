69d389d37b0581fa4123ed0ddc4e33c5c70a04b2
Francis Kodama <fk@fkodama.com>
2023-11-02 22:53:25 -0400

    Rotate key

--- app/lib/stock.server.ts
+++ app/lib/stock.server.ts
@@ -32,25 +32,3 @@ export const getStockUsd = async (symbol: string) => {
     return { error };
   }
 };
-
-// export const getStockUsd = async (symbol: string) => {
-//   try {
-//     const response = await fetch(
-//       // `https://serpapi.com/search.json?engine=google_finance&q=${symbol}&api_key=${process.env.NEXT_PUBLIC_SERPAPI}`
-//       `https://api.twelvedata.com/quote?symbol=${symbol}&apikey=${process.env.NEXT_PUBLIC_TWELVEDATA_KEY}`,
-//       {
-//         headers: {
-//           'Accept-Encoding': 'deflate',
-//           Authorization: `apikey ${process.env.NEXT_PUBLIC_TWELVEDATA_KEY}`,
-//         },
-//       }
-//     ).then((res) => res.json());
-//     return response;
-//   } catch (error) {
-//     return { error };
-//   }
-// };
-
-// const symbol = 'BVMF:IVVB11';
-// `https://serpapi.com/search.json?engine=google_finance&q=${symbol}&api_key=${process.env.NEXT_PUBLIC_SERPAPI}`
-// `https://api.twelvedata.com/time_series?symbol=${symbol}&interval=5min&apikey=${process.env.NEXT_PUBLIC_TWELVEDATA_KEY}`,
