d80b994fb326b88b6e5013f3f8a2797836fc0b0c
Francis Kodama <fk@fkodama.com>
2023-11-26 17:47:41 -0500

    Server action wip

--- components/AddAssetForm.tsx
+++ components/AddAssetForm.tsx
@@ -1,3 +1,32 @@
+import { useFormState, useFormStatus } from 'react-dom';
+import { Button } from './ui/button';
+
+//VERCEL: https://www.youtube.com/watch?v=dDpZfOQBMaU
+// indian https://www.youtube.com/watch?v=R_Pj593TH_Q
+
+const initialState = {
+  message: null,
+};
+
+const SubmitButton = ({ message }: any) => {
+  const { peding } = useFormStatus();
+
+  return (
+    <Button type='submit' aria-disabled={peding}>
+      Add Asset
+    </Button>
+  );
+};
+
 export const AddAssetForm = () => {
-  return <>Hello World!</>;
+  const [state, formAction] = useFormState(addAsset, initialState);
+  return (
+    <>
+      <form action={formAction}>
+        <label htmlFor='asset'>Enter Asset</label>
+        <input type='text' name='asset' id='asset' required />
+        <SubmitButton />
+      </form>
+    </>
+  );
 };
