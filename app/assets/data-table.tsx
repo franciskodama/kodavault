14a76cb5f87d36cae69205c1d86afd3a2ec0c2ba
Francis Kodama <fk@fkodama.com>
2023-11-28 21:48:56 -0500

    Change tsconfig paths

--- app/assets/data-table.tsx
+++ app/assets/data-table.tsx
@@ -17,8 +17,8 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from '../../components/ui/table';
-
+} from '@/components/ui/table';
+// } from '../../components/ui/table';
 import { AssetReducedWithAth } from '../lib/types';
 
 interface DataTableProps<TData, TValue> {
--- components/CardAllCurrencies.tsx
+++ components/CardAllCurrencies.tsx
@@ -7,7 +7,7 @@ import {
   CardDescription,
   CardHeader,
   CardTitle,
-} from './../components/ui/card';
+} from '@/components/ui/card';
 
 type totalArrayProps = {
   currency: string;
