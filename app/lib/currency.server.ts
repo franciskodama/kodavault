234e139963a34d8bb5698fccc6d82e3c987422d3
Francis Kodama <fk@fkodama.com>
2023-11-29 17:20:56 -0500

    Autoprefixer

--- app/lib/currency.server.ts
+++ app/lib/currency.server.ts
@@ -1,16 +1,16 @@
 export const getCurrency = async () => {
-  try {
-    const response = await fetch(
-      `http://apilayer.net/api/live?access_key=${process.env.NEXT_PUBLIC_APILAYER_KEY}`,
-      {
-        method: 'GET',
-        headers: {
-          'Accept-Encoding': 'deflate',
-        },
-      }
-    ).then((res) => res.json());
-    return response;
-  } catch (error) {
-    return { error };
-  }
+  // try {
+  //   const response = await fetch(
+  //     `http://apilayer.net/api/live?access_key=${process.env.NEXT_PUBLIC_APILAYER_KEY}`,
+  //     {
+  //       method: 'GET',
+  //       headers: {
+  //         'Accept-Encoding': 'deflate',
+  //       },
+  //     }
+  //   ).then((res) => res.json());
+  //   return response;
+  // } catch (error) {
+  //   return { error };
+  // }
 };
