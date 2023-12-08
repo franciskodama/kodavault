5fb40f7b34a28ea46f1f040aef989ada294bb70a
Francis Kodama <fk@fkodama.com>
2023-12-08 08:59:39 -0500

    Todo md file

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -56,42 +56,6 @@ export default async function ProtectedRoute() {
           cash: item.asset,
         }));
 
-      // Prompt AI: I have this app to manage my investments where I show a spreadsheet with each asset, with its proprieties, and also many cards to summarize specific data. Each card has a Title (name) and a description. I will have a card that shows the total amount of the whole vault in 3 different currencies (USD, CAD, BRL). How can I can this card and its description? It must be short.
-
-      // PRIORITIES:
-      // TODO: Input, Edit, Delete, new Asset
-      // TODO: If Asset has 0 total value, make the code more resilient so it doesn't crash
-
-      // FUTURE:
-      // https://css-generators.com/ribbon-shapes/
-      // TODO: to hover Total by Subtype and then see the next tier of details
-      // TODO: Generate Report + Send it by Email + Download it
-      // TODO: At the end of the Month close a Report
-      // TODO: On mouse over on Crypto, show the foundamentals explanation of this coin
-      // TODO: Search Asset
-      // TODO: Check this API: https://www.cryptometer.io/login.php
-
-      // BACKLOG:
-      // TODO: If there is cash on TFSA or HFSA, show it on the card or send a alert
-      // TODO: think about other alerts
-      // TODO: Place to save videos about coins for future reference
-      // TODO: Card with link of Data Analysis to check on the daily basis (Obdisian)
-      // TODO: Card with link for the image of the cycle of the market (already in the public folder)
-
-      //----------------------------------------------------------------------------------------------
-      // ATH CARD
-      // TODO: change all qty to qty
-      // TODO: alert: recommendation if the amount is too much for a little potential growth
-      // TODO: market cap
-      // TODO: Total By Crypto: show the percentage we want when we reach Bull Market
-      //----------------------------------------------------------------------------------------------
-
-      //----------------------------------------------------------------------------------------------
-      // DONE:
-      // TODO: Home beofre Dashboard - ref.: Kajabi
-      // TODO: Card "Next Purchases"
-      //----------------------------------------------------------------------------------------------
-
       return (
         <>
           <div className='flex flex-col gap-2'>
