34d5e2b1e2d5a45b61d588371a4335cda4421a16
Francis Kodama <fk@fkodama.com>
2023-12-10 14:55:23 -0500

    If Asset has 0 total value, make the code more resilient solution

--- components/CardAth/index.tsx
+++ components/CardAth/index.tsx
@@ -55,6 +55,35 @@ export const CardAth = async ({
     }, []);
 
     athAssets = sumQtyOfSameAssets.map((item: any) => {
+      // TODO: If Asset has 0 total value, make the code more resilient so it doesn't crash
+      // if (item.qty === 0) {
+      //   return {
+      //     asset: item.asset,
+      //     price: currencyFormatter(item.price),
+      //     qty: numberFormatter.format(item.qty),
+      //     currentTotal: currencyFormatter(0),
+      //     ath: currencyFormatter(item.ath),
+      //     athTotalNumber: 0,
+      //     athTotalCurrency: currencyFormatter(0),
+      //     xPotential: numberFormatter.format(0),
+      //     percentagePotential: numberFormatterNoDecimals.format(0),
+      //   };
+      // }
+
+      // return {
+      //   asset: item.asset,
+      //   price: currencyFormatter(item.price),
+      //   qty: numberFormatter.format(item.qty),
+      //   currentTotal: currencyFormatter(item.qty * item.price),
+      //   ath: currencyFormatter(item.ath),
+      //   athTotalNumber: item.ath * item.qty,
+      //   athTotalCurrency: currencyFormatter(item.ath * item.qty),
+      //   xPotential: numberFormatter.format(item.ath / item.price),
+      //   percentagePotential: numberFormatterNoDecimals.format(
+      //     ((item.ath - item.price) / item.price) * 100
+      //   ),
+      // };
+
       return {
         asset: item.asset,
         price: currencyFormatter(item.price),
