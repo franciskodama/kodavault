3c3a8fa0bfe476daf9d0093f3584a7ef21c028ec
Francis Kodama <fk@fkodama.com>
2023-12-10 13:01:56 -0500

    Google provider for auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -10,6 +10,10 @@ export const authOptions = {
       clientId: process.env.GITHUB_ID ?? '',
       clientSecret: process.env.GITHUB_SECRET ?? '',
     }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID ?? '',
+      clientSecret: process.env.GOOGLE_SECRET ?? '',
+    }),
   ],
   //   pages: {
   //     signIn: '/signin',
