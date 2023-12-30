764e106424194e53f235379611ff49b2dabd8e7d
Francis Kodama <fk@fkodama.com>
2023-12-29 23:26:23 -0500

    Clerk style, home page, and fix entry pages to dashboard

--- app/in/assets/assets.tsx
+++ app/in/assets/assets.tsx
@@ -9,6 +9,10 @@ import { AssetsContext } from '@/context/AssetsContext';
 export default function Assets() {
   const { assets } = useContext(AssetsContext);
 
+  // const { user } = useUser();
+  // const uid = user?.emailAddresses?.[0]?.emailAddress;
+  // console.log('---  🚀 ---> | user:', user?.emailAddresses[0].emailAddress);
+
   return (
     <div className='mx-auto'>
       <DataTable columns={columns} data={assets} />
