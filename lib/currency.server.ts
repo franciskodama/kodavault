b10d8e0fdcac806387b625682c3ad7cb4637b319
Francis Kodama <fk@fkodama.com>
2024-05-28 18:20:24 -0400

    Fix currency api

--- lib/currency.server.ts
+++ lib/currency.server.ts
@@ -4,6 +4,20 @@ export const getCurrency = async () => {
       `https://api.freecurrencyapi.com/v1/latest?apikey=${process.env.NEXT_PUBLIC_FREECURRENCYAPI}`,
       {
         method: 'GET',
+      }
+    ).then((res) => res.json());
+    return response;
+  } catch (error) {
+    return { error };
+  }
+};
+
+export const currencyRatesApiStatus = async () => {
+  try {
+    const response = await fetch(
+      `https://api.freecurrencyapi.com/v1/status?apikey=${process.env.NEXT_PUBLIC_FREECURRENCYAPI}`,
+      {
+        method: 'GET',
         headers: {
           'Accept-Encoding': 'deflate',
         },
@@ -14,20 +28,3 @@ export const getCurrency = async () => {
     return { error };
   }
 };
-
-// export const getCurrency = async () => {
-//   try {
-//     const response = await fetch(
-//       `http://apilayer.net/api/live?access_key=${process.env.NEXT_PUBLIC_APILAYER_KEY}`,
-//       {
-//         method: 'GET',
-//         headers: {
-//           'Accept-Encoding': 'deflate',
-//         },
-//       }
-//     ).then((res) => res.json());
-//     return response;
-//   } catch (error) {
-//     return { error };
-//   }
-// };
