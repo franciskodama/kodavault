6bb5ea04f5036fceb0a364c31feb2f64318338b6
Francis Kodama <fk@fkodama.com>
2024-06-17 21:11:12 -0400

    We have the beginning of an accordion

--- app/in/shortcut/shortcut.tsx
+++ app/in/shortcut/shortcut.tsx
@@ -24,24 +24,33 @@ export function Shortcut({ shortcuts }: { shortcuts: ShortcutType[] }) {
       <ul className='flex flex-col items-center justify-center mt-12 w-full text-sm'>
         {shortcuts.length > 0 &&
           shortcuts.map((shortcut: ShortcutType) => (
-            <li
-              key={shortcut.id}
-              className='flex items-center w-full text-left gap-2 mb-4'
-            >
-              <Link href={shortcut.url} target='_blank'>
-                <p className='w-[20em]'>{shortcut.name}</p>
-              </Link>
+            <Accordion key={shortcut.id} type='single' collapsible>
+              <AccordionItem value={shortcut.category}>
+                <AccordionTrigger>{shortcut.category}</AccordionTrigger>
+                <AccordionContent>
+                  Yes. It adheres to the WAI-ARIA design pattern.
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
 
-              <Separator orientation='vertical' />
-              {/* <Separator orientation='horizontal' /> */}
+            // <li
+            //   key={shortcut.id}
+            //   className='flex items-center w-full text-left gap-2 mb-4'
+            // >
+            //   <Link href={shortcut.url} target='_blank'>
+            //     <p className='w-[20em]'>{shortcut.name}</p>
+            //   </Link>
 
-              <div className='flex items-center justify-center h-[4ch] w-[14ch] bg-blue-400 rounded-[2px] text-white text-center'>
-                {shortcut.category}
-              </div>
-              <p className='flex items-center border w-1/3 ml-4 h-[4ch]'>
-                {shortcut.description}
-              </p>
-            </li>
+            //   <Separator orientation='vertical' />
+            //   {/* <Separator orientation='horizontal' /> */}
+
+            //   <div className='flex items-center justify-center h-[4ch] w-[14ch] bg-blue-400 rounded-[2px] text-white text-center'>
+            //     {shortcut.category}
+            //   </div>
+            //   <p className='flex items-center border w-1/3 ml-4 h-[4ch]'>
+            //     {shortcut.description}
+            //   </p>
+            // </li>
           ))}
       </ul>
     </div>
