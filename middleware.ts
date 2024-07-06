cab022f5c433868339722581f4e0b9fcd4fa2450
Francis Kodama <fk@fkodama.com>
2024-07-06 14:42:35 -0400

    Middleware from before

--- middleware.ts
+++ middleware.ts
@@ -1,28 +1,7 @@
-// import { authMiddleware } from '@clerk/nextjs';
-// import { NextRequest } from 'next/server';
-
-// export default authMiddleware({
-//   publicRoutes: ['/', '/api/cron-networth-evolution'],
-//   async beforeAuth(req: NextRequest, evt: any) {
-//     console.log('Request URL:', req.url);
-//   },
-// });
-
-// export const config = {
-//   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
-// };
-
 import { authMiddleware } from '@clerk/nextjs';
-import { NextRequest, NextResponse } from 'next/server';
 
 export default authMiddleware({
   publicRoutes: ['/', '/api/cron-networth-evolution'],
-  async beforeAuth(req: NextRequest) {
-    if (req.nextUrl.pathname === '/api/cron-networth-evolution') {
-      return NextResponse.next();
-    }
-    return NextResponse.next();
-  },
 });
 
 export const config = {
