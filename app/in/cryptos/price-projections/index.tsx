73cb3ba3735818b49bf952b84230e44f2b62e018
Francis Kodama <fk@fkodama.com>
2024-07-07 10:55:05 -0400

    Housekeeping

--- app/in/cryptos/price-projections/index.tsx
+++ app/in/cryptos/price-projections/index.tsx
@@ -20,54 +20,54 @@ export default function PriceProjections({ assets }: { assets: Asset[] }) {
 
   // const onlyCryptoAssets = assets.filter((item: any) => item.type === 'Crypto');
 
-  cryptoAssetsWithAth = assets.map((item: any) => {
-    const existingAsset = hardcodedAthCoins.find(
-      (el: any) => el.symbol === item.asset
-    );
-    return {
-      ...item,
-      ath: existingAsset?.ath ? existingAsset.ath : 0,
-    };
-  });
+  // cryptoAssetsWithAth = assets.map((item: any) => {
+  //   const existingAsset = hardcodedAthCoins.find(
+  //     (el: any) => el.symbol === item.asset
+  //   );
+  //   return {
+  //     ...item,
+  //     ath: existingAsset?.ath ? existingAsset.ath : 0,
+  //   };
+  // });
 
-  sumQtyOfSameAssets = cryptoAssetsWithAth.reduce((acc: any, item: any) => {
-    const existingAsset = acc.find((el: any) => el.asset === item.asset);
-    if (existingAsset) {
-      existingAsset.qty += item.qty;
-      existingAsset.currentTotal += item.total;
-    } else {
-      acc.push(item);
-    }
-    return acc;
-  }, []);
+  // sumQtyOfSameAssets = cryptoAssetsWithAth.reduce((acc: any, item: any) => {
+  //   const existingAsset = acc.find((el: any) => el.asset === item.asset);
+  //   if (existingAsset) {
+  //     existingAsset.qty += item.qty;
+  //     existingAsset.currentTotal += item.total;
+  //   } else {
+  //     acc.push(item);
+  //   }
+  //   return acc;
+  // }, []);
 
-  athAssets = sumQtyOfSameAssets.map((item: any) => {
-    return {
-      asset: item.asset,
-      price: currencyFormatter(item.price),
-      qty: numberFormatter.format(item.qty),
-      currentTotal: currencyFormatter(item.qty * item.price),
-      ath: currencyFormatter(item.ath),
-      athTotalNumber: item.ath * item.qty,
-      athTotalCurrency: currencyFormatter(item.ath * item.qty),
-      xPotential: numberFormatter.format(item.ath / item.price),
-      percentagePotential: numberFormatterNoDecimals.format(
-        ((item.ath - item.price) / item.price) * 100
-      ),
-    };
-  });
+  // athAssets = sumQtyOfSameAssets.map((item: any) => {
+  //   return {
+  //     asset: item.asset,
+  //     price: currencyFormatter(item.price),
+  //     qty: numberFormatter.format(item.qty),
+  //     currentTotal: currencyFormatter(item.qty * item.price),
+  //     ath: currencyFormatter(item.ath),
+  //     athTotalNumber: item.ath * item.qty,
+  //     athTotalCurrency: currencyFormatter(item.ath * item.qty),
+  //     xPotential: numberFormatter.format(item.ath / item.price),
+  //     percentagePotential: numberFormatterNoDecimals.format(
+  //       ((item.ath - item.price) / item.price) * 100
+  //     ),
+  //   };
+  // });
 
-  sortedAthAssets = athAssets.sort(
-    (a: AssetReducedWithAth, b: AssetReducedWithAth) => {
-      return Number(b.xPotential) - Number(a.xPotential);
-    }
-  );
+  // sortedAthAssets = athAssets.sort(
+  //   (a: AssetReducedWithAth, b: AssetReducedWithAth) => {
+  //     return Number(b.xPotential) - Number(a.xPotential);
+  //   }
+  // );
 
   return (
     <>
       {sortedAthAssets.length > 0 && (
         <div className='w-full'>
-          <DataTable athAssets={sortedAthAssets} />
+          {/* <DataTable athAssets={sortedAthAssets} /> */}
         </div>
       )}
     </>
