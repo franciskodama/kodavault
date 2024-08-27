89e2a81227814e9a60f94d8be863af358698b0b1
Francis Kodama <fk@fkodama.com>
2024-08-27 11:41:04 -0400

    Sort the data by date

--- app/in/dashboard/charts/net-worth.tsx
+++ app/in/dashboard/charts/net-worth.tsx
@@ -18,13 +18,7 @@ import { Loading } from '@/components/Loading';
 
 // https://www.react-google-charts.com/examples/line-chart
 
-type HeaderRowChartData = [
-  { type: 'date'; label: 'Day' },
-  string,
-  string,
-  string,
-  string
-];
+type HeaderRowChartData = [string, string, string, string, string];
 type RowChartData = [Date, number, number, number, number];
 type HeaderAndRowsChartData = [HeaderRowChartData, ...RowChartData[]];
 
@@ -33,9 +27,13 @@ export default function NetWorthChart({
 }: {
   netWorthChartData: netWorthChartData[];
 }) {
+  const sortedNetWorthChartData = netWorthChartData.sort((a, b) => {
+    return new Date(a.created_at).getTime() - new Date(b.created_at).getTime();
+  });
+
   const formattedData: HeaderAndRowsChartData = [
-    [{ type: 'date', label: 'Day' }, 'USD', 'CAD', 'BRL', 'BTC'],
-    ...(netWorthChartData.map((item) => [
+    ['', 'USD', 'CAD', 'BRL', 'BTC'],
+    ...(sortedNetWorthChartData.map((item) => [
       transformDateToYearMonth(item.created_at),
       +item.usd_total.toFixed(0),
       +item.cad_total.toFixed(0),
@@ -43,24 +41,20 @@ export default function NetWorthChart({
       +item.btc_total.toFixed(0) * 10000,
     ]) as RowChartData[]),
   ];
-  console.log('---  🚀 ---> | formattedData new:', formattedData);
 
-  const data = [
-    ['USD', 'CAD', 'BRL', 'BTC'],
-    [new Date(2014, 1), 0, 0, 5],
-    [new Date(2014, 2), 10, 5, 15],
-    [new Date(2014, 3), 23, 15, 35],
-    [new Date(2014, 4), 17, 9, 25],
-    [new Date(2014, 5), 18, 10, 45],
-    [new Date(2014, 6), 9, 5, 55],
-    [new Date(2014, 7), 11, 3, 35],
-    [new Date(2014, 8), 27, 19, 35],
-  ];
-  console.log('---  🚀 ---> | data:', data);
-
-  const options2 = {
-    // title: 'Age vs. Weight comparison',
-
+  const options = {
+    // width: 900,
+    height: 300,
+    chart: {
+      // title: 'Average Temperatures and Daylight in Iceland Throughout the Year',
+      // subtitle: 'in millions of dollars (USD)',
+    },
+    legend: { position: 'bottom' },
+    series: {
+      // Gives each series an axis name that matches the Y-axis below.
+      // 0: { axis: 'Temps' },
+      // 1: { axis: 'Daylight' },
+    },
     hAxis: {
       title: 'Weeks',
       //   viewWindow: { min: 0, max: 24 },
@@ -68,30 +62,6 @@ export default function NetWorthChart({
     vAxis: {
       title: '$',
       //   viewWindow: { min: 0, max: 3000000 },
-    },
-  };
-
-  //Options
-  //  https://www.react-google-charts.com/components/chart
-
-  const options1 = {
-    chart: {
-      title: 'Box Office Earnings in First Two Weeks of Opening',
-      subtitle: 'in millions of dollars (USD)',
-    },
-    legend: { position: 'bottom' },
-  };
-
-  const options = {
-    chart: {
-      // title: 'Average Temperatures and Daylight in Iceland Throughout the Year',
-    },
-    width: 900,
-    height: 500,
-    series: {
-      // Gives each series an axis name that matches the Y-axis below.
-      0: { axis: 'Temps' },
-      1: { axis: 'Daylight' },
     },
     axes: {
       // Adds labels to each axis; they don't have to match the axis names.
@@ -101,6 +71,9 @@ export default function NetWorthChart({
       },
     },
   };
+
+  //Options
+  //  https://www.react-google-charts.com/components/chart
 
   return (
     <>
@@ -143,5 +116,5 @@ const transformDateToYearMonth = (date: Date | string): Date => {
   if (typeof date === 'string') {
     return new Date(date);
   }
-  return new Date(date.getFullYear(), date.getMonth());
+  return new Date(date.getFullYear(), date.getMonth(), date.getDate());
 };
