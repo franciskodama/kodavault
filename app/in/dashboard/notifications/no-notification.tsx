ffbddad718ad297ed28e38a676641fb5948b5738
Francis Kodama <fk@fkodama.com>
2024-09-06 17:12:46 -0400

    We have a no notifications card

---
+++ app/in/dashboard/notifications/no-notification.tsx
@@ -0,0 +1,55 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { PiggyBank } from 'lucide-react';
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Asset } from '@/lib/types';
+import { Button } from '@/components/ui/button';
+
+export default function NoNotifications() {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push('/in/retirement');
+  };
+
+  return (
+    <>
+      <Card className='h-[240px]'>
+        <div className='flex flex-col justify-between h-full'>
+          <div className='flex flex-col'>
+            <CardHeader>
+              <CardTitle className='capitalize flex items-center justify-between'>
+                <span>Notification Desert</span>
+                <span className='text-3xl'>📭</span>
+              </CardTitle>
+              <CardDescription className='text-xs'>
+                Your inbox is blissfully empty.
+              </CardDescription>
+            </CardHeader>
+            <CardContent className='relative'>
+              <h3 className='font-bold my-1'>Quiet inbox?</h3>
+              <p className='pr-2'>
+                Enjoy seeing how close you are to your retirement goal.
+              </p>
+            </CardContent>
+          </div>
+          <CardFooter className='flex justify-between text-sm text-slate-500 font-medium mx-1 px-2 pb-3'>
+            <Button size='md' onClick={handleClick}>
+              <PiggyBank size={16} className='mr-2' />
+              Find your Goal
+            </Button>
+          </CardFooter>
+        </div>
+      </Card>
+    </>
+  );
+}
--- app/in/dashboard/notifications/notifications-panel.tsx
+++ app/in/dashboard/notifications/notifications-panel.tsx
@@ -12,6 +12,7 @@ import {
 } from '@/components/ui/card';
 import { Asset } from '@/lib/types';
 import CashAlert from './cash-alert';
+import NoNotifications from './no-notification';
 
 export default function NotificationsPanel({ cash }: { cash: Asset[] }) {
   return (
@@ -19,6 +20,7 @@ export default function NotificationsPanel({ cash }: { cash: Asset[] }) {
       <div className='flex flex-col gap-2 flex-1'>
         {/* What happen if there is no cash to invest? What we will show? */}
         {cash.length > 0 && <CashAlert cash={cash} />}
+        {cash.length < 1 && <NoNotifications />}
 
         <Card className='h-[240px]'>
           <div className='flex flex-col justify-between h-full'>
