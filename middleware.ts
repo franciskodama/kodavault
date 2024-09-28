451a0244f76bfc3a690edc0ce907877372819865
Francis Kodama <fk@fkodama.com>
2024-09-28 11:34:55 -0400

    Housekeeping in the middleware file

--- middleware.ts
+++ middleware.ts
@@ -1,26 +1,3 @@
-// import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
-
-// const isProtectedRoute = createRouteMatcher(['/in(.*)', '/[[...rest]](.*)']);
-
-// console.log('Number -----> ', Math.random(), '------------------------------');
-
-// export default clerkMiddleware((auth, req) => {
-//   if (isProtectedRoute(req)) auth().protect();
-// });
-
-// export const config = {
-//   matcher: [
-//     // Skip Next.js internals and all static files, unless found in search params
-//     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-//     // Always run for API routes
-//     // '/(api|trpc)',
-//   ],
-// };
-
-// ------------------------------------------------------------------------------------------------
-// Protect all routes
-// To protect all routes in your application and define specific routes as public, you can use any of the above methods and simply invert the if condition.
-
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
 const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)']);
@@ -31,29 +8,6 @@ export default clerkMiddleware((auth, req) => {
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    // '/(api|trpc)',
   ],
 };
-
-// import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
-
-// const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)']);
-
-// export default clerkMiddleware((auth, request) => {
-//   if(!isPublicRoute(request)) {
-//     auth().protect();
-//   }
-// });
-
-// export const config = {
-//   matcher: [
-//     // Skip Next.js internals and all static files, unless found in search params
-//     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-//     // Always run for API routes
-//     '/(api|trpc)(.*)',
-//   ],
-// };
-// ------------------------------------------------------------------------------------------------
