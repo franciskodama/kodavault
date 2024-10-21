15f34ab05fbb3e5bb1de736360c750f1272dd934
Francis Kodama <fk@fkodama.com>
2024-10-21 16:23:22 -0400

    Disabled fields until you get type on add asset

--- components/AddAssetForm.tsx
+++ components/AddAssetForm.tsx
@@ -175,169 +175,176 @@ export function AddAssetForm() {
           </ul>
         </div>
 
-        <div className='flex flex-col'>
-          {assetSymbol && fixedSymbolsArr.includes(assetSymbol) ? null : (
+        {assetSubtype && (
+          <div className='flex flex-col'>
+            {assetSymbol && fixedSymbolsArr.includes(assetSymbol) ? null : (
+              <div className={classDiv}>
+                <label className={classTitle} htmlFor='asset'>
+                  Asset
+                </label>
+                <input
+                  className={classInput}
+                  placeholder='Asset Symbol'
+                  {...register('asset', { required: "Asset can't be empty" })}
+                />
+                {errors.asset?.message && (
+                  <p className={classError}>{errors.asset.message}</p>
+                )}
+              </div>
+            )}
+
             <div className={classDiv}>
-              <label className={classTitle} htmlFor='asset'>
-                Asset
+              <label className={classTitle} htmlFor='qty'>
+                Quantity
               </label>
               <input
                 className={classInput}
-                placeholder='Asset Symbol'
-                {...register('asset', { required: "Asset can't be empty" })}
+                placeholder='Quantity'
+                {...register('qty', { required: "Quantity can't be empty" })}
               />
-              {errors.asset?.message && (
-                <p className={classError}>{errors.asset.message}</p>
+              {errors.qty?.message && (
+                <p className={classError}>{errors.qty.message}</p>
               )}
             </div>
-          )}
 
-          <div className={classDiv}>
-            <label className={classTitle} htmlFor='qty'>
-              Quantity
-            </label>
-            <input
-              className={classInput}
-              placeholder='Quantity'
-              {...register('qty', { required: "Quantity can't be empty" })}
-            />
-            {errors.qty?.message && (
-              <p className={classError}>{errors.qty.message}</p>
+            <div className={classDiv}>
+              <h3 className={classTitle}>Wallet</h3>
+              <ul className={classUl}>
+                {assetWallet.map((walletOption) => (
+                  <li key={walletOption}>
+                    <input
+                      className='hidden peer'
+                      type='radio'
+                      value={walletOption}
+                      id={walletOption}
+                      {...register('wallet')}
+                    />
+                    <label className={classLabelRadio} htmlFor={walletOption}>
+                      <span>{walletOption}</span>
+                    </label>
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            {assetCategory.length > 1 && (
+              <div className={classDiv}>
+                <h3 className={classTitle}>Category</h3>
+                <ul className={classUl}>
+                  {categoryOptions.map((categoryOption) => (
+                    <li key={categoryOption}>
+                      <TooltipProvider>
+                        <Tooltip>
+                          <TooltipTrigger>
+                            <input
+                              className='hidden peer'
+                              type='radio'
+                              value={categoryOption}
+                              id={categoryOption}
+                              {...register('category')}
+                            />
+
+                            <label
+                              className={classLabelRadio}
+                              htmlFor={categoryOption}
+                            >
+                              <span>{categoryOption}</span>
+                            </label>
+                          </TooltipTrigger>
+                          <TooltipContent>
+                            <p className='text-xs w-[20ch]'>
+                              {getCategoryTooltip(categoryOption)}
+                            </p>
+                          </TooltipContent>
+                        </Tooltip>
+                      </TooltipProvider>
+                    </li>
+                  ))}
+                </ul>
+              </div>
             )}
-          </div>
 
-          <div className={classDiv}>
-            <h3 className={classTitle}>Wallet</h3>
-            <ul className={classUl}>
-              {assetWallet.map((walletOption) => (
-                <li key={walletOption}>
-                  <input
-                    className='hidden peer'
-                    type='radio'
-                    value={walletOption}
-                    id={walletOption}
-                    {...register('wallet')}
-                  />
-                  <label className={classLabelRadio} htmlFor={walletOption}>
-                    <span>{walletOption}</span>
-                  </label>
-                </li>
-              ))}
-            </ul>
-          </div>
-
-          {assetCategory.length > 1 && (
             <div className={classDiv}>
-              <h3 className={classTitle}>Category</h3>
+              <h3 className={classTitle}>Purpose</h3>
               <ul className={classUl}>
-                {categoryOptions.map((categoryOption) => (
-                  <li key={categoryOption}>
-                    <TooltipProvider>
-                      <Tooltip>
-                        <TooltipTrigger>
-                          <input
-                            className='hidden peer'
-                            type='radio'
-                            value={categoryOption}
-                            id={categoryOption}
-                            {...register('category')}
-                          />
-
-                          <label
-                            className={classLabelRadio}
-                            htmlFor={categoryOption}
-                          >
-                            <span>{categoryOption}</span>
-                          </label>
-                        </TooltipTrigger>
-                        <TooltipContent>
-                          <p className='text-xs w-[20ch]'>
-                            {getCategoryTooltip(categoryOption)}
-                          </p>
-                        </TooltipContent>
-                      </Tooltip>
-                    </TooltipProvider>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          )}
-
-          <div className={classDiv}>
-            <h3 className={classTitle}>Purpose</h3>
-            <ul className={classUl}>
-              {purposeOptions.map((purposeOption) => (
-                <li key={purposeOption}>
-                  <input
-                    className='hidden peer'
-                    type='radio'
-                    value={purposeOption}
-                    id={purposeOption}
-                    {...register('purpose')}
-                  />
-                  <label className={classLabelRadio} htmlFor={purposeOption}>
-                    <span>{purposeOption}</span>
-                  </label>
-                </li>
-              ))}
-            </ul>
-          </div>
-
-          <div className={classDiv}>
-            <label className={classTitle} htmlFor='tag'>
-              Tag
-            </label>
-            <input
-              className={classInput}
-              placeholder='Tag the asset, if needed.'
-              {...register('tag')}
-            />
-          </div>
-
-          {assetCurrency.length > 1 && (
-            <div className={classDiv}>
-              <h3 className={classTitle}>Currency</h3>
-              <ul className={classUl}>
-                {assetCurrency.map((currencyOption) => (
-                  <li key={currencyOption}>
+                {purposeOptions.map((purposeOption) => (
+                  <li key={purposeOption}>
                     <input
                       className='hidden peer'
                       type='radio'
-                      value={currencyOption}
-                      id={currencyOption}
-                      {...register('currency')}
+                      value={purposeOption}
+                      id={purposeOption}
+                      {...register('purpose')}
                     />
-                    <label className={classLabelRadio} htmlFor={currencyOption}>
-                      <span>{currencyOption}</span>
+                    <label className={classLabelRadio} htmlFor={purposeOption}>
+                      <span>{purposeOption}</span>
                     </label>
                   </li>
                 ))}
               </ul>
             </div>
-          )}
 
-          {assetAccount.length > 1 && (
             <div className={classDiv}>
-              <h3 className={classTitle}>Account</h3>
-              <ul className={classUl}>
-                {assetAccount.map((accountOption) => (
-                  <li key={accountOption}>
-                    <input
-                      className='hidden peer'
-                      type='radio'
-                      value={accountOption}
-                      id={accountOption}
-                      {...register('account')}
-                    />
-                    <label className={classLabelRadio} htmlFor={accountOption}>
-                      <span>{accountOption}</span>
-                    </label>
-                  </li>
-                ))}
-              </ul>
+              <label className={classTitle} htmlFor='tag'>
+                Tag
+              </label>
+              <input
+                className={classInput}
+                placeholder='Tag the asset, if needed.'
+                {...register('tag')}
+              />
             </div>
-          )}
-          {/* 
+
+            {assetCurrency.length > 1 && (
+              <div className={classDiv}>
+                <h3 className={classTitle}>Currency</h3>
+                <ul className={classUl}>
+                  {assetCurrency.map((currencyOption) => (
+                    <li key={currencyOption}>
+                      <input
+                        className='hidden peer'
+                        type='radio'
+                        value={currencyOption}
+                        id={currencyOption}
+                        {...register('currency')}
+                      />
+                      <label
+                        className={classLabelRadio}
+                        htmlFor={currencyOption}
+                      >
+                        <span>{currencyOption}</span>
+                      </label>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            )}
+
+            {assetAccount.length > 1 && (
+              <div className={classDiv}>
+                <h3 className={classTitle}>Account</h3>
+                <ul className={classUl}>
+                  {assetAccount.map((accountOption) => (
+                    <li key={accountOption}>
+                      <input
+                        className='hidden peer'
+                        type='radio'
+                        value={accountOption}
+                        id={accountOption}
+                        {...register('account')}
+                      />
+                      <label
+                        className={classLabelRadio}
+                        htmlFor={accountOption}
+                      >
+                        <span>{accountOption}</span>
+                      </label>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            )}
+            {/* 
           We don't need this for now (until we don't pay for an API to get the data
           Today we get in from Google Sheet - Google Finance formula)
           
@@ -363,16 +370,17 @@ export function AddAssetForm() {
             </div>
           )} */}
 
-          <Button className='mt-8' type='submit'>
-            Add Asset
-          </Button>
-
-          <SheetClose asChild>
-            <Button ref={closeRef} className='my-4' variant='outline'>
-              Close
+            <Button className='mt-8' type='submit'>
+              Add Asset
             </Button>
-          </SheetClose>
-        </div>
+
+            <SheetClose asChild>
+              <Button ref={closeRef} className='my-4' variant='outline'>
+                Close
+              </Button>
+            </SheetClose>
+          </div>
+        )}
       </form>
     </>
   );
