73fc2d53a9563e2eb307ff93cb923b407f6d94f1
Francis Kodama <fk@fkodama.com>
2025-03-02 18:11:26 -0500

    We have a page

--- app/in/elliott/page.tsx
+++ app/in/elliott/page.tsx
@@ -1,8 +1,33 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+
 export default function Elliott() {
   return (
     <div className='flex flex-col w-full gap-2'>
-      <div className='flex flex-col sm:flex-row gap-2 px-8 sm:px-0'>
-        Elliott
+      <div className='w-full'>
+        <Card>
+          <div className='flex flex-col justify-between'>
+            <div className='flex flex-col'>
+              <CardHeader>
+                <CardTitle className='capitalize flex items-center justify-between'>
+                  <span>Elliott Perspective</span>
+                  <span className='text-3xl mr-4'>🧠</span>
+                </CardTitle>
+                <CardDescription className='text-xs'>
+                  Decoding psychology, enhancing risk-return.
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <div>Content</div>
+              </CardContent>
+            </div>
+          </div>
+        </Card>
       </div>
     </div>
   );
