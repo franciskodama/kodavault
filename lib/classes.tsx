b7c8c329c125998c0485b9ce46a692a3313f0e70
Francis Kodama <fk@fkodama.com>
2026-02-20 15:43:47 -0500

    Fix width sheet to add and update asset

--- lib/classes.tsx
+++ lib/classes.tsx
@@ -6,9 +6,9 @@ export const tableHeaderClass =
 export const classInput =
   'border border-slate-200 h-10 p-2 rounded-xs w-full mt-2';
 export const classDiv = 'my-4';
-export const classUl = 'flex flex-wrap gap-2';
+export const classUl = 'grid grid-cols-3 gap-2';
 export const classTitle = 'font-bold mb-2';
 export const classError =
   'px-3 py-1 text-white bg-red-500 font-semibold border border-white';
 export const classLabelRadio =
-  'inline-flex items-center justify-center py-1 w-[8em] h-[2.5em] border-2 rounded-[2px] cursor-pointer text-primary border-gray-200 peer-checked:font-bold peer-checked:border-slate-500 peer-checked:text-primary peer-checked:bg-accent hover:text-slate-600 hover:bg-gray-100';
+  'inline-flex items-center justify-center py-1 w-full h-[2.5em] border-2 rounded-md cursor-pointer text-primary border-gray-200 peer-checked:font-bold peer-checked:border-slate-500 peer-checked:text-primary peer-checked:bg-accent hover:text-slate-600 hover:bg-gray-100';
